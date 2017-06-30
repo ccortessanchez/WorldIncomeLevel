e1980e32abffa029a9838569115d8a904f092430
Carlos Cortés Sánchez <ccortes@gmv.es>
2017-06-30 13:26:54 +0200

    Added pop over area list menu

--- WorldIncomeLevel/ViewController.swift
+++ WorldIncomeLevel/ViewController.swift
@@ -9,14 +9,28 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, MKMapViewDelegate, UIPopoverPresentationControllerDelegate {
 
     @IBOutlet weak var areaListButton: UIBarButtonItem!
     @IBOutlet weak var mapView: MKMapView!
     
+    var contentController: UITableViewController!
+    var areaListTable: UITableView!
+    var selectedItemIndex: Int!
+    
+    var regionNames = ["Africa", "East Asia and the Pacific", "Central Europe and the Baltics", "Europe and Central Asia", "Latin America and the Caribbean", "Middle East and North Africa", "South Asia", "European Union", "North America", "North Africa", "South Asia"]
+    var regionCodes = ["AFR", "CEA","CEB","CEU","CLA","CME","CSA","EUU","NAC","NAF","SAS"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        contentController = UITableViewController()
+        areaListTable = UITableView()
+        areaListTable.dataSource = self
+        areaListTable.delegate = self
+        contentController.tableView = areaListTable
+        selectedItemIndex = -1
+        mapView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +39,26 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAreaList(_ sender: Any) {
+        contentController.modalPresentationStyle = UIModalPresentationStyle.popover
+        let popOverPC: UIPopoverPresentationController = contentController.popoverPresentationController!
+        popOverPC.barButtonItem = areaListButton
+        popOverPC.permittedArrowDirections = UIPopoverArrowDirection.any
+        popOverPC.delegate = self
+        present(contentController, animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell:UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier")
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.value1, reuseIdentifier: "cellIdentifier")
+            cell.textLabel?.text = regionNames[indexPath.row]
+        }
+        cell.accessoryType = UITableViewCellAccessoryType.none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return regionNames.count
     }
 
 }
